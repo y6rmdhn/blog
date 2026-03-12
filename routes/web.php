ab29138b837743327e2ae11177259a750e35105f
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2026-03-13 04:09:45 +0700

    blade templating engine

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',  ["nama" => "yopan"]);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
